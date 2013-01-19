23b4cd641046ad4c6e3c5a67380d0c2529cb010c
Alvin Cris Tabontabon <tabs5894@gmail.com>
2013-01-19 02:38:59 +0200

    Organized the scene layout

--- src/tabs/FXDialog.java
+++ src/tabs/FXDialog.java
@@ -1,22 +1,44 @@
 package tabs;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class FXDialog extends Application {
-    
-    @Override
-    public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXDialogUI.fxml"));
-        
-        Scene scene = new Scene(root);
-        
-        stage.setScene(scene);
-        stage.show();
-    }
 
-    public static void main(String[] args) {  launch(args); }
+    private double initX;
+    private double initY;
+
+    @Override
+    public void start(final Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXDialogUI.fxml"));
+        Scene scene = new Scene(root, Color.TRANSPARENT);
+
+        stage.centerOnScreen();
+        stage.setScene(scene);
+        stage.initStyle(StageStyle.TRANSPARENT);
+        stage.show();
+
+        root.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent me) {
+                initX = me.getScreenX() - stage.getX();
+                initY = me.getScreenY() - stage.getY();
+            }
+        });
+
+        root.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent me) {
+                stage.setX(me.getScreenX() - initX);
+                stage.setY(me.getScreenY() - initY);
+            }
+        });
+    }
 }
