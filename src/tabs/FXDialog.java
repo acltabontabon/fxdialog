230f552ca520bbe759108db69296057e73866cdf
Alvin Cris Tabontabon <tabs5894@gmail.com>
2013-01-19 02:54:49 +0200

    Added a transition

--- src/tabs/FXDialog.java
+++ src/tabs/FXDialog.java
@@ -1,6 +1,9 @@
 package tabs;
 
+import javafx.animation.ScaleTransition;
+import javafx.animation.ScaleTransitionBuilder;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -9,21 +12,29 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import javafx.util.Duration;
 
 public class FXDialog extends Application {
 
     private double initX;
     private double initY;
+    
+    private static Parent root;
+    private static Stage primaryStage;
 
     @Override
     public void start(final Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXDialogUI.fxml"));
+        primaryStage = stage; 
+        
+        root = FXMLLoader.load(getClass().getResource("FXDialogUI.fxml"));
         Scene scene = new Scene(root, Color.TRANSPARENT);
 
         stage.centerOnScreen();
         stage.setScene(scene);
         stage.initStyle(StageStyle.TRANSPARENT);
         stage.show();
+        
+        scaleTransition(0, 0, 1, 1, 1);
 
         root.setOnMousePressed(new EventHandler<MouseEvent>() {
             @Override
@@ -38,6 +49,28 @@ public class FXDialog extends Application {
             public void handle(MouseEvent me) {
                 stage.setX(me.getScreenX() - initX);
                 stage.setY(me.getScreenY() - initY);
+            }
+        });
+    }
+
+    protected static void scaleTransition(double fromX, double fromY, double toX, double toY, double dur) {
+        ScaleTransition scale = ScaleTransitionBuilder.create()
+                .node(root)
+                .duration(Duration.seconds(dur))
+                .fromX(fromX)
+                .fromY(fromY)
+                .toX(toX)
+                .toY(toY)
+                .build();
+        scale.play();
+        
+        scale.setOnFinished(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent t) {
+                if(root.getScaleX() == 0) {
+                    primaryStage.close();
+                }
             }
         });
     }
--- src/tabs/FXDialogUIController.java
+++ src/tabs/FXDialogUIController.java
@@ -2,6 +2,8 @@ package tabs;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
 public class FXDialogUIController implements Initializable {
@@ -9,5 +11,9 @@ public class FXDialogUIController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    }   
+    
+    @FXML private void ok(ActionEvent evt) {
+        FXDialog.scaleTransition(1, 1, 0, 0, 0.5);
+    }
 }
